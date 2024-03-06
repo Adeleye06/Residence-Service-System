71b73ccf01a76b48e6e728f846d25be0271579a5
AlexT1259 <tangjingcheng1259@icloud.com>
2024-03-06 15:47:38 -0700

    a checking page that shows if user have filled a form before or fill a new form

---
+++ getForm.php
@@ -0,0 +1,38 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['studentEmail'])){
+    header("refresh:3; url=student_login.php");
+    die("you did not log in, going to student log in page in 3 seconds");
+}
+
+print "welcome user".$_SESSION['studentEmail']."<br>";
+
+print "this page checks if user filled forms and print them if they had, or let user to fill a new form<br>";
+
+$conn = new mysqli("172.22.2.116", "res", "Password1", "residence", "1433");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT FORM_FILLED.FILLED_FORM_ID FROM FORM_USER INNER JOIN FORM_FILLED WHERE U_ID = {$_SESSION['U_ID']} AND FORM_ID = {$_GET['id']};";
+print $sql."<br>";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    while ($row = $result->fetch_assoc()) {
+        print "we have find you already filled the form with id:";
+       print $row['FILLED_FORM_ID']."  ";
+       print "click here to go to see the form <a href='formDetail.php?filled={$row['FILLED_FORM_ID']}'>click me</a>";
+       print "<br>";
+    }
+
+}else{
+    print "you have not filled this form yet";
+}
+
+print "please fill this form with link <a href='formDetail.php?form={$_GET['id']}'>this link please</a>";
+
+?>
