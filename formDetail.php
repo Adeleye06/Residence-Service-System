361c873af4c65106223f7604fbd1a8a639ffdcf2
AlexT1259 <tangjingcheng1259@icloud.com>
2024-03-06 16:03:13 -0700

    added basic of drawing the form for filling

--- formDetail.php
+++ formDetail.php
@@ -8,12 +8,36 @@ if(!isset($_SESSION['studentEmail'])){
 
 
 if (isset($_GET['form'])){
-    print "you are trying to fill a new form";
+    print "you are trying to fill a new form<br><br><form>";
+
+    $conn = new mysqli("172.22.2.116", "res", "Password1", "residence", "1433");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    
+    $sql = "SELECT * FROM QUESTION WHERE FORM_ID = {$_GET['form']}";
+
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0){
+        while ($row = $result->fetch_assoc()) {
+           print "question number ".$row['Q_ID']."<br>";
+           print "question is ".$row['Q_HTML']."<br>";
+           print "answer is ".$row['A_HTML']."<br><br>";;
+        }
+
+    }else{
+        die("we did not find any question in the database, there is a error");
+    }
+
+    print "<button type='submit'>submit</button></form>";
     die();
 }
 
 if (isset($_GET['filled'])){
-    print "you are trying to view a filled form";
+    print "you are trying to view a filled form<br><br>";
     die();
 }
 
