7ab731938182371a1856fefa3f19b6c87e2ec42e
yuvman <113378618+yuvman@users.noreply.github.com>
2024-03-21 22:28:21 -0600

    Getform, saved form, newform css changes
    confirmed changes

--- forms/getForm.php
+++ forms/getForm.php
@@ -27,14 +27,10 @@
 </head>
 <body>
 <header class="site-header">
-<<<<<<< Updated upstream
-    <img src="../assets/images/lc-logo.png" alt="Lethbridge College Logo" class="logo">
-=======
 <a href="index.php">
     <img src="../assets/images/lc-logo.png" alt="Lethbridge College Logo" class="logo">
 </a>
 
->>>>>>> Stashed changes
     <nav>
         <ul>
             <li><a href="../logout.php">Logout</a></li>
@@ -122,16 +118,6 @@
     </div>
 </div>
 <footer class="site-footer">
-<<<<<<< Updated upstream
-    <img src="../assets/images/lc-logo.png" alt="Lethbridge College Logo" class="footer-logo">
-    <p>3000 College Dr S, Lethbridge, Alberta, Canada, T1K 1L6</p>
-    <p>1-800-572-0103</p>
-    <nav>
-        <a href="contact.php">Contacts and Maps</a>
-    </nav>
-</footer>
-
-=======
 <a href="index.php">
     <img src="../assets/images/lc-logo.png" alt="Lethbridge College Logo" class="logo">
 </a>
@@ -144,6 +130,5 @@
     </nav>
 </footer>
 
->>>>>>> Stashed changes
 </body>
 </html>
